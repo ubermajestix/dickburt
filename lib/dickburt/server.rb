90897c04a6e6c2d77217b3b944c955024cfaa9d5
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2011-08-29 16:18:10 -0600

    args not opts

--- lib/dickburt/server.rb
+++ lib/dickburt/server.rb
@@ -12,7 +12,7 @@ module Dickburt
           puts "="*45
           puts "Connected to #{@campfire.host} with token #{@campfire.token}"
           puts "="*45
-          @room = @campfire.rooms.detect{|r| r.name == opts[:room]}
+          @room = @campfire.rooms.detect{|r| r.name == args[:room]}
           @room.join
           puts "Ready for messages..."
           puts "="*45
